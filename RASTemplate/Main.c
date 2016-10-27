ee11003ecb42e20a200ab8bef772a14834672d9a
bcshang <bcshang@utexas.edu>
2016-10-26 18:33:14 -0700

    Preliminary code for servo and sensors done

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,24 +1,111 @@
 #include <RASLib/inc/common.h>
-#include <RASLib/inc/gpio.h>
-#include <RASLib/inc/time.h>
+#include <RASLib/inc/gpio.h>//Pin help
+#include <RASLib/inc/time.h>//Timer help
+#include <RASLib/inc/servo.h>//Servo help
+#include <RASLib/inc/adc.h>//IR Sensor help
+#include <RASLib/inc/time.h>//Time help
 
-// Blink the LED to show we're on
+/* Brandon
+ * Robot Car Program
+ * 		Rasware Driven
+ *
+ * LED Colors: F2:Blue
+ *			   F3:Green
+ *			   F4:Red
+ *
+ * Pin Specifications:
+ *		Right Servo:B1
+ *		Left Servo:	B2
+ *
+ *		Right IR Sensor:
+ *		Left IR Sensor: 
+ */
+
+
+/*~~~~~Blinking to show program running~~~~~*/
 tBoolean blink_on = true;
-
 void blink(void) {
-    SetPin(PIN_F3, blink_on);
+	//Signifies the program is running, F2 is Blue LED
+    SetPin(PIN_F2, blink_on);
     blink_on = !blink_on;
 }
+/*~~~~~Blinking to show program running~~~~~*/
 
 
-// The 'main' function is the entry point of the program
-int main(void) {
-    // Initialization code can go here
-    CallEvery(blink, 0, 0.5);
+/*~~~~~~~~~~~~~Servo Code~~~~~~~~~~~~~~~~~~~*/
+static tServo *servo_R, *servo_L;
+static tBoolean Servo_initd = false;
+
+void initServo(void) {
+    // don't initialize this if we've already done so
+    if (Servo_initd) {
+        return;
+    }
+    Servo_initd = true;
+    servo_R = InitializeServo(PIN_B0);
+    servo_L = InitializeServo(PIN_B1);
+}
+
+ //Movement functions
+ static float speed = .3;
+ void moveForward(void) {
+ 	SetServo(servo_L, 1-speed);
+    SetServo(servo_R, speed);
+ }
+ void moveBackward(void) {
+ 	SetServo(servo_L, speed);
+ 	SetServo(servo_R, 1-speed);
+ }
+ void moveStop(void) {
+ 	SetServo(servo_L, .5);
+ 	SetServo(servo_R, .5);
+ }
+
+ //TODO requires calibration
+ void moveRight(void) {
+ 	SetServo(servo_R, speed);
+ 	SetServo(servo_L, speed);
+ 	Wait(1.5);
+ 	moveStop();
+ }
+ void moveLeft(void) {
+ 	SetServo(servo_R, 1-speed);
+ 	SetServo(servo_L, 1-speed);
+ 	Wait(1.5);
+ 	moveStop();
+ }
+/*~~~~~~~~~~~~Servo Code End~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~IR Sensor Code End~~~~~~~~~~~~~~~*/
+static tADC *adc[2];
+static tBoolean IR_initd = false;
+void initIRSensor(void) {
+    // don't initialize this if we've already done so
+    if (IR_initd) {
+        return;
+    }
     
+    IR_initd = true;
+
+    // initialize 4 pins to be used for ADC input
+    adc[0] = InitializeADC(PIN_D0);
+    adc[1] = InitializeADC(PIN_D1);
+}
+/*~~~~~~~~~~~~IR Sensor Code End~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~Main Code Start~~~~~~~~~~~~~~~~*/
+int main(void) {
+	//Function to say the program is running
+    CallEvery(blink, 0, 1);
+
+    //Initialize servos and IR sensors
+    initServo();
+    initIRSensor();
+
     while (1) {
-        // Runtime code can go here
-        Printf("Hello World!\n");
-        
+        Printf("Speed is %1.3f\n", speed);
+        Wait(5);
     }
 }
